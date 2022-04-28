7b6c330d94d3db137c196a0bf9a9059a7e7a5147
Mohit Rana <mohitrana04032003@gmail.com>
2022-04-28 22:46:52 +0530

    moviesdb

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+// function index() {
+//   return (
+//     <div>
+//       index
+//       <App />
+//     </div>
+//   );
+// }
+
+ReactDom.render(<App />, document.getElementById("root"));
