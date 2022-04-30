41b290e05d7f5513d418ebec41e0701527df4cea
Mohit Rana <mohitrana04032003@gmail.com>
2022-04-30 19:29:49 +0530

    add navbar and footer error in design

---
+++ real-estate/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => (
+  <Box
+    textAlign="centre"
+    p="5"
+    color="gray.600"
+    borderTop="1px"
+    borderColor="grey.100"
+  >
+    2022 Realtor,Inc
+  </Box>
+);
+
+export default Footer;
--- real-estate/pages/index.js
+++ real-estate/pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Button, Text } from "@chakra-ui/react";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
+import Property from "../components/Property";
 
 const Banner = ({
   purpose,
@@ -40,7 +41,7 @@ const Banner = ({
 );
 
 export default function Home({ propertiesForSale, propertiesForRent }) {
-  console.log(propertiesForSale, propertiesForRent);
+  // console.log(propertiesForSale, propertiesForRent);
   return (
     <Box>
       <h1>Hello</h1>
@@ -54,7 +55,11 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
-      <Flex flexWrap="wrap">{/* fetch the properties */}</Flex>
+      <Flex flexWrap="wrap">
+        {propertiesForRent.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
       <Banner
         purpose="BUY A HOME"
         title1=" Find, Buy & Own Your"
@@ -65,7 +70,11 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      <Flex flexWrap="wrap">{/* fetch the properties */}</Flex>
+      <Flex flexWrap="wrap">
+        {propertiesForSale.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
     </Box>
   );
 }
