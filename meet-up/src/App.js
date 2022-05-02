f32afa96b7b3944303c67b4edbac0660e4235c2b
Mohit Rana <mohitrana04032003@gmail.com>
2022-05-02 07:43:30 +0530

    outputting list

---
+++ meet-up/src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/MainNavigation";
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ meet-up/src/pages/AllMeetups.js
@@ -0,0 +1,43 @@
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+];
+
+function AllMeetupsPage() {
+  return (
+    <section>
+      <h1>All Meet Ups</h1>
+      {/* list to jsx */}
+      {/* {[<li>Item 1</li>, <li>Item 2</li>]} */}
+      {/* use map for rendering each element */}
+
+      <ul>
+        {[
+          // we need to give id because otherwise it will give error
+          DUMMY_DATA.map((meetup) => {
+            return <li id={meetup.id}> {meetup.title}</li>;
+            // <img src={meetup.image} alt="none" />;
+          }),
+        ]}
+      </ul>
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
