6e1ea11fa15b21650dd3637ee6be55c86065a497
Mohit Rana <mohitrana04032003@gmail.com>
2022-05-03 08:50:18 +0530

    useNavigate -useHistory and navigating

--- meet-up/src/pages/NewMeetup.js
+++ meet-up/src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useNavigate } from "react-router-dom";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
   function onAddMeetupHandler(meetupData) {
     // for sending the http request we can use the fetch function it is a built in js function not a react. it allows to send a http request. We can also use axios but it is third party package fro sending http request
     // fetch is a get request
@@ -15,7 +18,10 @@ function NewMeetupPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      // we can use replace of push but using replace it won't allow to go back
+      navigate("/", { replace: true });
+    });
 
     // most api stores fetch req so to store a post req to we add another paramter in fetch
   }
